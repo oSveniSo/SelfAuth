8f566c58c502d41eaf0348e49e7dd08d9e91e248
Sven Mora <sven.mora@hotmail.com>
2023-05-31 11:31:43 +0200

    Initial Commit

---
+++ SelfAuth/SelfAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfAuthApp.swift
+//  SelfAuth
+//
+//  Created by Sven Mora on 31.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SelfAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
